69e2e2597f78fc0916461c1252712d1b8666a086
GiselleBarbosa <g88barbosa@gmail.com>
2025-01-13 22:22:03 -0300

    initial commit

---
+++ src/main/java/br/com/barbosa/FinancesMsTransactionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.barbosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesMsTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancesMsTransactionsApplication.class, args);
+    }
+
+}
