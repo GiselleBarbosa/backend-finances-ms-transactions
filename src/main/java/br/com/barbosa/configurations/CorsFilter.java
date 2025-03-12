4349a8728594eae2fa57f323311beee7c18ad523
GiselleBarbosa <g88barbosa@gmail.com>
2025-03-11 22:19:51 -0300

    config: atualiza CorsFilter

--- src/main/java/br/com/barbosa/configurations/CorsFilter.java
+++ src/main/java/br/com/barbosa/configurations/CorsFilter.java
@@ -1,4 +1,4 @@
-package br.com.barbosa.configurations;
+package com.seuapp.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,14 +11,15 @@ import java.io.IOException;
 
 @Component
 public class CorsFilter extends OncePerRequestFilter {
+
     @Override
-    protected void doFilterInternal(
-            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        response.setHeader("Access-Control-Allow-Origin", "controle-financeiro-xi-beryl.vercel.app");
+        response.setHeader("Access-Control-Allow-Origin", "https://controle-financeiro-xi-beryl.vercel.app");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
