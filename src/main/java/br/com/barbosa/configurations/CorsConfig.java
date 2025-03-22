2d6ad205c6298229e1a06f31ea53f0983678ea50
GiselleBarbosa <g88barbosa@gmail.com>
2025-03-22 18:25:59 -0300

    config(CORS): ajuste de CORS

--- src/main/java/br/com/barbosa/configurations/CorsConfig.java
+++ src/main/java/br/com/barbosa/configurations/CorsConfig.java
@@ -10,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedOrigins("http://localhost:4200", "https://finances-peach-sigma.vercel.app")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
-
     }
 }
