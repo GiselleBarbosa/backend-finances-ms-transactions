dbaa8c1d228b421cf1aeb3dabbc3e214e08ba436
GiselleBarbosa <g88barbosa@gmail.com>
2025-03-24 09:11:13 -0300

    config(controllers): remove annotation dos controllers

--- src/main/java/br/com/barbosa/configurations/CorsConfig.java
+++ src/main/java/br/com/barbosa/configurations/CorsConfig.java
@@ -4,15 +4,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://finances-peach-sigma.vercel.app")
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
--- src/main/java/br/com/barbosa/controllers/TransactionController.java
+++ src/main/java/br/com/barbosa/controllers/TransactionController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/transactions")
 public class TransactionController {
 
