0fd30853b0fe31346c5afba6f4f253ee93b67cb5
GiselleBarbosa <g88barbosa@gmail.com>
2025-04-20 22:16:08 -0300

    wip

--- src/main/java/br/com/barbosa/configurations/CorsConfig.java
+++ src/main/java/br/com/barbosa/configurations/CorsConfig.java
@@ -14,10 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
+                        .allowedOrigins("https://finances-fawn.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
-
             }
         };
     }
