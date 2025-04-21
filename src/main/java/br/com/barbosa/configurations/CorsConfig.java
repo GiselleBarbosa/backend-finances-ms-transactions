02ea201470c82a6b75ce25e472fa2971cc0af0c6
GiselleBarbosa <g88barbosa@gmail.com>
2025-04-21 14:16:35 -0300

    wip: adiciona content-type

--- src/main/java/br/com/barbosa/configurations/CorsConfig.java
+++ src/main/java/br/com/barbosa/configurations/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://finances-fawn.vercel.app", "http://localhost:4200")
+                        .allowedOrigins("https://finances-fawn.vercel.app/", "http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Content-Type", "Authorization")
                         .allowCredentials(false);
